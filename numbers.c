44af90913aaf2d49b2b343a983ea3f8829e00246
niylii <173621209+niylii@users.noreply.github.com>
2024-11-12 22:35:50 +0100

    numbers conversion and handling [update]

--- numbers.c
+++ numbers.c
@@ -5,14 +5,16 @@
  * @nb : the number to convert
  */
 
-void  binary(unsigned int nb)
+int  binary(unsigned int nb)
 {
 	int bi[32];
 	int i = 0, j;
+	int count = 0;
 
 	if (nb == 0)
 	{
 		putchar('0');
+		return (1);
 	}
 	while (nb > 0)
 	{
@@ -24,5 +26,71 @@ void  binary(unsigned int nb)
 	while (j >= 0)
 	{
 		putchar(bi[j--] + '0');
+		count++;
 	}
-}  
+	return (count);
+}
+
+/**
+ * print_nbr - function that printd an intiger as a character
+ * @nb : the number
+ */
+
+int print_nbr(int n)
+{
+	int count = 0;
+	int div = 1;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+		count++;
+	}
+	while (n / div > 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		putchar((n / div) + '0');
+		n %= div;
+		div /= 10;
+		count++;
+	}
+	return(count);
+}
+
+/**
+ * print_uns_nbr - function that makes an unsigned intiger as a character
+ * @nb : the number
+ */
+
+int print_uns_nbr(unsigned int n)
+{
+	int count = 0;
+	int div = 1;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	while (n / div > 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		putchar((n / div) + '0');
+		n %= div;
+		div /= 10;
+		count++;
+	}
+	return (count);
+}
