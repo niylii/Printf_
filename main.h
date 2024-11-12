ac43f81ff978125f04dbc3a13b987855103c25ae
niylii <173621209+niylii@users.noreply.github.com>
2024-11-12 20:13:29 +0100

    header file [update - task 2]

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h> 
-#include <stdio.h> 
+#define MIAN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+void binary(unsigned int nb);
 #endif
