61ffd395409f4c7db0202a0e15e2b33753b6f894
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-11-14 06:29:10 +0100

    [update - task 3 done]

--- numbers.c
+++ numbers.c
@@ -94,3 +94,106 @@ int print_uns_nbr(unsigned int n)
 	}
 	return (count);
 }
+
+/**
+ * octal - function that converts the number to octal
+ * @n : the number 
+ * Return: the number in octal
+ */
+
+int octal(unsigned int n)
+{
+	int i = 0, count = 0;
+	int j;
+	char oc[32];
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		oc[i++] = ((n % 8) + '0');
+		n /= 8;
+	}
+	j = i - 1;
+	while (j >= 0)
+	{
+		putchar(oc[j--]);
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * hexa - function that converts the number to hexadecimal
+ * @n : the number 
+ * Return: the number in hexadecimal
+ */
+
+int hexa(unsigned int n)
+{
+	int i = 0, count = 0;
+	int j, mod;
+	char he[32];
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		mod = n % 16;
+		if (mod < 10)
+			he[i++] = (mod + '0');
+		else
+			he[i++] = ((mod - 10) + 'a');
+		n /= 16;
+	}
+	j = i - 1;
+	while (j >= 0)
+	{
+		putchar(he[j--]);
+		count++;
+	}
+	return (count);
+}
+
+
+/**
+ * hexaup - function that converts the number to hexadecimal(UPPERCASE)
+ * @n : the number 
+ * Return: the number in hexadecimal
+ */
+
+int hexaup(unsigned int n)
+{
+	int i = 0, count = 0;
+	int j, mod;
+	char he[32];
+
+	if (n == 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		mod = n % 16;
+		if (mod < 10)
+			he[i++] = (mod + '0');
+		else
+			he[i++] = ((mod - 10) + 'A');
+		n /= 16;
+	}
+	j = i - 1;
+	while (j >= 0)
+	{
+		putchar(he[j--]);
+		count++;
+	}
+	return (count);
+}
+
