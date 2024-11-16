73504f76ebcc77d1c0b2a11f523abd595eec6f13
nouhailaaleen <173621209+niylii@users.noreply.github.com>
2024-11-16 17:59:23 +0100

    functions that prints the numbers

---
+++ number.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * print_nbr - function that printd an intiger as a character
+ * @nb : the number
+ * Return: the lenght
+ */
+
+int print_nbr(int n)
+{
+	int count = 0;
+	int div = 1;
+	char c;
+
+	if (n == 0)
+	{
+		write (1, "0", 1);
+		return (1);
+	}
+	if (n < 0)
+	{
+		write (1, "-", 1);
+		n = -n;
+		count++;
+	}
+	while (n / div > 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		c = (n / div) + '0';
+		write (1, &c, 1);
+		n %= div;
+		div /= 10;
+		count++;
+	}
+	return(count);
+}
+
+/**
+ * print_uns_nbr - function that makes an unsigned intiger as a character
+ * @nb : the number
+ * Return: the lenght
+ */
+
+int print_uns_nbr(unsigned int n)
+{
+	int count = 0;
+	unsigned int div = 1;
+	char c;
+
+	if (n == 0)
+	{
+		write (1, "0", 1);
+		return (1);
+	}
+	while (n / div >= 10)
+	{
+		div *= 10;
+	}
+	while (div > 0)
+	{
+		c = (n / div) + '0';
+		write (1, &c, 1);
+		n %= div;
+		div /= 10;
+		count++;
+	}
+	return (count);
+}
